30f2b2628ea08016a230a8f89b0bcd6f60b6a133
manyshhh <manyshhh@gmail.com>
2021-04-12 00:04:07 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 function rate(){
-    var ratevalue=document.getElementById("interestRate");
+    var ratevalue=document.getElementById("interestrate");
     document.getElementById("r").innerHTML=ratevalue.value+"%";
   }
 
   function compute(){
-    var ratevalue=parseFloat(document.getElementById("interestRate").value);
+    event.preventDefault();
+    var ratevalue=parseFloat(document.getElementById("interestrate").value);
     var principal=parseFloat(document.getElementById("amount").value);
     var time=parseFloat(document.getElementById("years").value);
     var total= principal*(1+((ratevalue)*(time)));
-    if(document.getElementById("amount").value==""){
-      alert("Please enter the principal amount");
+    if((principle.value == "") || (principle.value < 1)){
+          alert("Please enter a positive number");
+          principle.focus();
+          return false;
+          return true;
     }
+  
     var today=new Date();
     var year=today.getFullYear();
     time+=year;
